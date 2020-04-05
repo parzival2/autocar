2b7c84d4d42d7eacf4a860108119bf581e0131da
kalyan_sreenivas <r.kalyan.sreenivas@gmail.com>
2020-04-05 10:18:04 +0200

    Added description Added a gazebo plugin that controls the ackermann steering

---
+++ autocar_ackermann_plugin/include/ackermann_plugin.hh
@@ -0,0 +1,154 @@
+#include <ackermann_msgs/AckermannDrive.h>
+#include <gazebo/common/common.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo_plugins/gazebo_ros_utils.h>
+#include <ignition/math/Vector3.hh>
+#include <ros/callback_queue.h>
+#include <ros/ros.h>
+#include <thread>
+
+namespace gazebo
+{
+class AckermannPlugin : public ModelPlugin
+{
+  public:
+    /**
+     *   Reset all the values if the AckermannDrive message doesnt arrive after this time
+     *   has passed
+     */
+    static constexpr double DEFAULT_RESET_TIME = 5.0;
+    /**
+     * @brief The DriveJoints enum Enum representing the drivable joints in our vehicle.
+     */
+    enum class DriveJoints : uint8_t
+    {
+        LF = 0,
+        RF = 1,
+        LR = 2,
+        RR = 3,
+        Count = 4
+    };
+    /**
+     * @brief The SteerJoints enum Enum representing the steerable joints in our vehicle.
+     */
+    enum class SteerJoints : uint8_t
+    {
+        LF = 0,
+        RF = 1,
+        Count = 2
+    };
+
+    AckermannPlugin();
+    ~AckermannPlugin();
+    // Overload the load method
+    void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) override;
+
+  private:
+    // Variables
+    /**
+     * @brief mGazeboRosPtr Pointer to the gazebo ros
+     */
+    GazeboRosPtr mGazeboRosPtr;
+    // Model
+    physics::ModelPtr mModelPtr;
+    /**
+     * @brief mRosNodeHandle Ros node handle
+     */
+    std::unique_ptr<ros::NodeHandle> mRosNodeHandle;
+    /**
+     * @brief mRosQueue Callback queue for ros messages
+     */
+    ros::CallbackQueue mRosQueue;
+    // Subscribers
+    /**
+     * @brief AckermannDrive command subscriber for receiving messages.
+     */
+    ros::Subscriber mAckermannMsgSub;
+    /**
+     * @brief mRosQueueThread A Thread to process messages in queue.
+     */
+    std::thread mRosQueueThread;
+    /**
+     * @brief mUpdateConnection Pointer to the update event
+     */
+    event::ConnectionPtr mUpdateConnection;
+    /**
+     * @brief mAckermannCommandTopic The topic on which to listen for AckermannDrive messages.
+     */
+    std::string mAckermannCommandTopic;
+    /**
+     * @brief mDriveJointNames Array of strings that represent the drivable joints in the model.
+     */
+    std::string mDriveJointNames[static_cast<uint8_t>(DriveJoints::Count)];
+    /**
+     * @brief mSteerJointNames Array of strings that represent the steerable joints in the model.
+     */
+    std::string mSteerJointNames[static_cast<uint8_t>(SteerJoints::Count)];
+    /**
+     * @brief mSteerJoints Vector of steerable joints represented by JointPtr
+     */
+    std::vector<physics::JointPtr> mSteerJoints;
+    /**
+     * @brief mDriveJoints Vector of drivable joints represented by JointPtr
+     */
+    std::vector<physics::JointPtr> mDriveJoints;
+    /**
+     * @brief mDrivePids PID controllers used to control the drivable joints LR, RR
+     */
+    std::vector<common::PID> mDrivePids;
+    /**
+     * @brief mSteerPids PID controllers used to control the steerable joints LF, RF
+     */
+    std::vector<common::PID> mSteerPids;
+    /**
+     * @brief mReferenceWheelSpeeds Reference wheel speeds that we will try to acheive.
+     * These will be calculated when we receive the AckermannDrive message.
+     */
+    std::vector<double> mReferenceWheelSpeeds;
+    /**
+     * @brief mReferenceSteerAngles Reference steering wheel angles that we will try to acheive
+     * These will be calculated when we receive the AckermannDrive message.
+     */
+    std::vector<double> mReferenceSteerAngles;
+    /**
+     * @brief mReferenceAckermannMsg The message that is received on the callback. This will be
+     * the reference state that we will try to achieve.
+     */
+    ackermann_msgs::AckermannDrive::ConstPtr mReferenceAckermannMsgPtr;
+    /**
+     * @brief mLastUpdateTime The last update time to calculate the time difference between the
+     * updates.
+     */
+    common::Time mLastUpdateTime;
+    /**
+     * @brief mLastAckermannCmdTime Just for keeping track of whether we have received any ackermann
+     * command message.
+     */
+    common::Time mLastAckermannCmdTime;
+    /**
+     * @brief mWheelBase Wheelbase that we receive from model plugin parameters.
+     */
+    double mWheelBase;
+    /**
+     * @brief mWheelSeperation Wheel seperation that we receive from model plugin parameters.
+     */
+    double mWheelSeperation;
+    /**
+     * @brief mWheelDiameter Wheel diameter.
+     */
+    double mWheelDiameter;
+    /**
+     * @brief QueueThread Function to process messages in queue
+     */
+    void QueueThread();
+    /**
+     * @brief ackermannCallback Callback for the AckermannDrive message
+     * @param ackermann_msg AckermannDrive message
+     */
+    void ackermannCallback(const ackermann_msgs::AckermannDrive::ConstPtr& ackermann_msg);
+    /**
+     * @brief OnUpdate Will be called when the gazebo simulation updates.
+     */
+    void OnUpdate();
+};
+} // namespace gazebo
---
+++ autocar_ackermann_plugin/src/ackermann_plugin.cpp
@@ -0,0 +1,220 @@
+#include "ackermann_plugin.hh"
+
+namespace gazebo
+{
+// Constructor
+AckermannPlugin::AckermannPlugin() {}
+
+// Destructor
+AckermannPlugin::~AckermannPlugin() {}
+
+/**
+ * @brief AckermannPlugin::Load This function will be called when Gazebo first loads the plugin.
+ * @param _model Pointer to the model.
+ * @param _sdf Pointer to the SDF element tree.
+ */
+void AckermannPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
+    // Gazebo ros instance.
+    this->mGazeboRosPtr = GazeboRosPtr(new GazeboRos(_model, _sdf, "AckermannPlugin"));
+
+    // Model
+    this->mModelPtr = _model;
+
+    // Check if ros is initialized.
+    this->mGazeboRosPtr->isInitialized();
+
+    this->mUpdateConnection =
+        event::Events::ConnectWorldUpdateBegin(std::bind(&AckermannPlugin::OnUpdate, this));
+
+    // Get the strings representing the drivable joints in our vehicle.
+    /// Left Front wheel drivable joint
+    this->mDriveJointNames[static_cast<uint8_t>(DriveJoints::LF)] =
+        _sdf->Get<std::string>("LF_driveJoint");
+    ROS_INFO_STREAM("AckermannPlugin : Provided Left Front drivable joint : "
+                    << mDriveJointNames[static_cast<uint8_t>(DriveJoints::LF)]);
+    /// Right Front wheel drivable joint
+    this->mDriveJointNames[static_cast<uint8_t>(DriveJoints::RF)] =
+        _sdf->Get<std::string>("RF_driveJoint");
+    ROS_INFO_STREAM("AckermannPlugin : Provided Right Front drivable joint : "
+                    << mDriveJointNames[static_cast<uint8_t>(DriveJoints::RF)]);
+    /// Left Rear wheel drivable joint
+    this->mDriveJointNames[static_cast<uint8_t>(DriveJoints::LR)] =
+        _sdf->Get<std::string>("LR_driveJoint");
+    ROS_INFO_STREAM("AckermannPlugin : Provided Left Rear drivable joint : "
+                    << mDriveJointNames[static_cast<uint8_t>(DriveJoints::LR)]);
+    /// Right Rear wheel drivable joint
+    this->mDriveJointNames[static_cast<uint8_t>(DriveJoints::RR)] =
+        _sdf->Get<std::string>("RR_driveJoint");
+    ROS_INFO_STREAM("AckermannPlugin : Provided Right Rear drivable joint : "
+                    << mDriveJointNames[static_cast<uint8_t>(DriveJoints::RR)]);
+    ///
+    /// Steering joints
+    ///
+    this->mSteerJointNames[static_cast<uint8_t>(SteerJoints::LF)] =
+        _sdf->Get<std::string>("LF_steerJoint");
+    ROS_INFO_STREAM("AckermannPlugin : Provided Left Front steering joint : "
+                    << mSteerJointNames[static_cast<uint8_t>(SteerJoints::LF)]);
+    this->mSteerJointNames[static_cast<uint8_t>(SteerJoints::RF)] =
+        _sdf->Get<std::string>("RF_steerJoint");
+    ROS_INFO_STREAM("AckermannPlugin : Provided Right Front steering joint : "
+                    << mSteerJointNames[static_cast<uint8_t>(SteerJoints::RF)]);
+    ////
+    //// Wheel base and Wheel seperation
+    ////
+    mWheelBase = _sdf->Get<double>("wheelBase");
+    ROS_INFO_STREAM("AckermannPlugin : Wheel base received : " << mWheelBase);
+    mWheelSeperation = _sdf->Get<double>("wheelSeperation");
+    ROS_INFO_STREAM("AckermannPlugin : Wheel seperation received : " << mWheelSeperation);
+
+    ////
+    //// Wheel diameter
+    ////
+    mWheelDiameter = _sdf->Get<double>("wheelDiameter");
+    ROS_INFO_STREAM("AckermannPlugin : Wheel diameter received : " << mWheelDiameter);
+
+    // Initialize the drivable joints
+    mDriveJoints.resize(static_cast<uint8_t>(DriveJoints::Count));
+    mReferenceWheelSpeeds.resize(static_cast<uint8_t>(DriveJoints::Count));
+    mReferenceWheelSpeeds.assign(static_cast<uint8_t>(DriveJoints::Count), 0.0);
+    mSteerJoints.resize(static_cast<uint8_t>(SteerJoints::Count));
+    mReferenceSteerAngles.resize(static_cast<uint8_t>(SteerJoints::Count));
+    mReferenceSteerAngles.assign(static_cast<uint8_t>(SteerJoints::Count), 0.0);
+    // PIDS
+    mDrivePids.resize(static_cast<uint8_t>(DriveJoints::Count));
+    mSteerPids.resize(static_cast<uint8_t>(SteerJoints::Count));
+    // Fill up the pointers for drive joints
+    for(size_t i = 0; i < mDriveJoints.size(); i++)
+    {
+        mDriveJoints[i] = mModelPtr->GetJoint(mDriveJointNames[i]);
+        mDrivePids[i].Init(0.8, 0.1, 0.8, 5.0, 0.0, 10.0, -10.0);
+    }
+    // Fill up the pointers for steer joints
+    for(size_t j = 0; j < mSteerJoints.size(); j++)
+    {
+        mSteerJoints[j] = mModelPtr->GetJoint(mSteerJointNames[j]);
+        mSteerPids[j].Init(100.0, 0.1, 10.0, 5.0, 0.0, 1.5, -1.5);
+    }
+    // Create a new node
+    if(!ros::isInitialized())
+    {
+        int argc = 0;
+        char** argv = NULL;
+        ros::init(argc, argv, "ackermann_plugin_node", ros::init_options::NoSigintHandler);
+    }
+    this->mRosNodeHandle.reset(new ros::NodeHandle("ackermann_plugin_node"));
+    // Initialize the last update time
+    mLastUpdateTime = mModelPtr->GetWorld()->SimTime();
+    mLastAckermannCmdTime = mModelPtr->GetWorld()->SimTime();
+
+    ros::SubscribeOptions so = ros::SubscribeOptions::create<ackermann_msgs::AckermannDrive>(
+        "/ackermann_cmd", 1, boost::bind(&AckermannPlugin::ackermannCallback, this, _1),
+        ros::VoidPtr(), &this->mRosQueue);
+    this->mAckermannMsgSub = this->mRosNodeHandle->subscribe(so);
+    ROS_INFO_STREAM("Plugin Initialized");
+    ROS_INFO_STREAM("Name of the model : " << this->mModelPtr->GetName().c_str());
+    this->mRosQueueThread = std::thread(std::bind(&AckermannPlugin::QueueThread, this));
+}
+
+/**
+ * @brief AckermannPlugin::QueueThread Function to process messages in queue.
+ */
+void AckermannPlugin::QueueThread()
+{
+    static const double timeout = 0.01;
+    while(this->mRosNodeHandle->ok())
+    {
+        this->mRosQueue.callAvailable((ros::WallDuration(timeout)));
+    }
+}
+
+/**
+ * @brief AckermannPlugin::OnUpdate This function will be called whenever gazebo updates its
+ * simulation.
+ */
+void AckermannPlugin::OnUpdate()
+{
+    common::Time currentTime = mModelPtr->GetWorld()->SimTime();
+    common::Time deltaTime = currentTime - mLastUpdateTime;
+    common::Time deltaAckremannMsg = currentTime - mLastAckermannCmdTime;
+    if(deltaAckremannMsg.Double() > DEFAULT_RESET_TIME)
+    {
+        mReferenceWheelSpeeds.assign(static_cast<uint8_t>(DriveJoints::Count), 0.0);
+        mReferenceSteerAngles.assign(static_cast<uint8_t>(SteerJoints::Count), 0.0);
+    }
+    double wheelDia_2 = 0.5 * mWheelDiameter;
+    if(deltaTime > 0.01)
+    {
+        // Set the reference speeds for wheels
+        // Convert rad/sec to m/sec by multiplying the angular velocity with radius
+        // v = r x omega
+        /// Wheels
+        /// Left rear
+        double leftRearSpeed =
+            mDriveJoints[static_cast<uint8_t>(DriveJoints::LR)]->GetVelocity(2) * wheelDia_2;
+        double leftRearSpeedError =
+            leftRearSpeed - mReferenceWheelSpeeds[static_cast<uint8_t>(DriveJoints::LR)];
+        double leftRearCommandEffort =
+            mDrivePids[static_cast<uint8_t>(DriveJoints::LR)].Update(leftRearSpeedError, deltaTime);
+        mDriveJoints[static_cast<uint8_t>(DriveJoints::LR)]->SetForce(2, leftRearCommandEffort);
+        /// Right rear
+        double rightRearSpeed =
+            mDriveJoints[static_cast<uint8_t>(DriveJoints::RR)]->GetVelocity(2) * wheelDia_2;
+        double rightRearSpeedError =
+            rightRearSpeed - mReferenceWheelSpeeds[static_cast<uint8_t>(DriveJoints::RR)];
+        double rightRearCommandEffort = mDrivePids[static_cast<uint8_t>(DriveJoints::RR)].Update(
+            rightRearSpeedError, deltaTime);
+        mDriveJoints[static_cast<uint8_t>(DriveJoints::RR)]->SetForce(2, rightRearCommandEffort);
+        ///
+        /// Steering angles
+        /// Left front
+        for(size_t i = 0; i < mSteerJoints.size(); i++)
+        {
+            double currentSteeringAngle = mSteerJoints[static_cast<uint8_t>(i)]->Position(0);
+            double steeringAngleError =
+                currentSteeringAngle - mReferenceSteerAngles[static_cast<uint8_t>(i)];
+            double steerCommandEffort =
+                mSteerPids[static_cast<uint8_t>(i)].Update(steeringAngleError, deltaTime);
+            mSteerJoints[static_cast<uint8_t>(i)]->SetForce(0, steerCommandEffort);
+        }
+        mLastUpdateTime = currentTime;
+    }
+}
+
+/**
+ * @brief AckermannPlugin::ackermannCallback Callback message for ackermann_cmd topic
+ * @param ackermann_msg AckermannDrive message.
+ */
+void AckermannPlugin::ackermannCallback(
+    const ackermann_msgs::AckermannDrive::ConstPtr& ackermann_msg)
+{
+    // Set the time for ackermann command message
+    mLastAckermannCmdTime = mModelPtr->GetWorld()->SimTime();
+    this->mReferenceAckermannMsgPtr = ackermann_msg;
+    // Calculate the steering angles taking into account the wheel base and wheel seperation.
+    double numerator = static_cast<double>(2.0) * mWheelBase * sin(ackermann_msg->steering_angle);
+    double denomLF = (static_cast<double>(2.0) * mWheelBase * cos(ackermann_msg->steering_angle)) -
+                     (mWheelSeperation * sin(ackermann_msg->steering_angle));
+    double denomRF = (static_cast<double>(2.0) * mWheelBase * cos(ackermann_msg->steering_angle)) +
+                     (mWheelSeperation * sin(ackermann_msg->steering_angle));
+    mReferenceSteerAngles.assign(static_cast<uint8_t>(SteerJoints::Count), 0.0);
+    mReferenceSteerAngles[static_cast<uint8_t>(SteerJoints::LF)] = atan2(numerator, denomLF);
+    mReferenceSteerAngles[static_cast<uint8_t>(SteerJoints::RF)] = atan2(numerator, denomRF);
+    // Calculate the wheel speeds
+    mReferenceWheelSpeeds.assign(static_cast<uint8_t>(DriveJoints::Count), 0.);
+    double spNumLR =
+        static_cast<double>(1.0) - (mWheelSeperation * tan(ackermann_msg->steering_angle));
+    double spNumRR =
+        static_cast<double>(1.0) + (mWheelSeperation * tan(ackermann_msg->steering_angle));
+    double spDenom = static_cast<double>(2.0) * mWheelBase;
+    mReferenceWheelSpeeds[static_cast<uint8_t>(DriveJoints::LR)] =
+        ackermann_msg->speed * (spNumLR / spDenom);
+    mReferenceWheelSpeeds[static_cast<uint8_t>(DriveJoints::RR)] =
+        ackermann_msg->speed * (spNumRR / spDenom);
+    ROS_DEBUG_STREAM("Received ackermann message");
+    ROS_DEBUG_STREAM("Reference speed : " << ackermann_msg->speed);
+    ROS_DEBUG_STREAM("Reference angle : " << ackermann_msg->steering_angle);
+}
+// Register this plugin with the simulator
+GZ_REGISTER_MODEL_PLUGIN(AckermannPlugin)
+} // namespace gazebo
